a9c12c93bee8fc34cc806675b462d3dff4a0235e
AaronPengwp <weiping_551@163.com>
2019-01-10 21:49:04 +0800

    视图函数

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,23 @@ def show_time(requset):
     t = time.ctime()
 
     return render(requset,"index.html",{"t":t}) #locals()
+
+
+def article_year(request,y):
+
+    return HttpResponse(y)
+
+def article_year_month(request,year,month):
+
+    return HttpResponse("year:%s  month:%s"%(year,month))
+
+
+
+def register(request):
+
+    if request.method=="POST":
+        print(request.POST.get("user"))
+        print(request.POST.get("age"))
+        return HttpResponse("success!")
+
+    return render(request,"register.html")
